87bebdb054372fa44b685f31e85d634abf350854
Monnus <maun2velsket@gmail.com>
2022-10-03 13:32:39 -0700

    file display container

--- src/component/onLoadBoxes/Boxes.js
+++ src/component/onLoadBoxes/Boxes.js
@@ -1,20 +1,17 @@
 import React,{useState} from 'react'
 import { Box } from '@mui/system'
-import { LinearProgress } from '@mui/material';
-LinearProgress
+
 const Boxes = ({color="gray",title}) => {
     const [count,setCount]=useState(0);
   return (
-    <Box sx={{background:color,width:"20rem",height:"10rem"}}>
+    <Box sx={{background:color,width:"20rem",height:"10rem",lineHeight:"20px"}}>
 <h2 style={{color:"white"}}>
     {title}
     </h2>
-<br/>
 <p style={{fontSize:"30px",color:"white"}}>
     {count}
     </p>
-    Progress bars
-
+ <Box sx={{marginLeft:"10px",height:"30px",backgroundColor:"lightgray",width:"90%"}}></Box>
    </Box>
   )
 }
--- src/component/upload/uplaodload.js
+++ src/component/upload/uplaodload.js
@@ -7,7 +7,7 @@ import Boxes from '../onLoadBoxes/Boxes';
 function displayUplaodTable(){
     console.log("hello");
     return(
-        <Box sx={{width:"100vw",height:"50vh",backgroundColor:"rgb(255, 255, 167)"}}>
+        <Box sx={{width:"100vw",height:"50vh",backgroundColor:"#F5F5F5"}}>
 
     </Box>
   )
@@ -15,17 +15,18 @@ function displayUplaodTable(){
 const welcomeCard=()=>{
     return(
         <Box style={{height:"100%",width:"50%",display:"flex",alignItems:"center",flexDirection:"column",justifyContent:"space-around"}}>       
-<Box sx={{display:"flex" ,borderBottom:"2px solid rgba(128,128,128,0.4)",padding:"7px",minWidth:"30em",maxWidth:"800px"
+<Box sx={{display:"flex" ,borderBottom:"2px solid rgba(128,128,128,0.4)",padding:"7px",minWidth:"37em",maxWidth:"800px"
 ,height:"20vh",backgroundColor:"#ECF5FF",alignItems:"center",justifyContent:"space-between",flexDirection:"row"}}>
     <Box 
     sx={{display:"flex",flexDirection:"column",textAlign:"start",lineHeight:"30x"}}>
 <Box component="h2">Welcome Back, </Box>
 <p style={{fontSize:"20px"}}>get the power to take your BlockDoc to the next level</p>
     </Box>
-<img src={Welocome} alt="welcom image" style={{width:"200px",height:"200px",alignSelf:"end",}}/>
+<img src={Welocome} alt="welcom" style={{width:"200px",height:"200px",alignSelf:"end",}}/>
 </Box>
-    <Box sx={{display:"flex" ,border:"2px dotted rgba(128,128,128,0.4)"
-    ,height:"20vh",minWidth:"37em",maxWidth:"800px",backgroundColor:"#ECF5FF",alignItems:"center",justifyContent:"center",flexDirection:"column"}}>
+    <Box sx={{display:"flex" ,border:"4px dotted rgba(128,128,128,0.4)"
+    ,height:"20vh",minWidth:"40em",maxWidth:"800px",backgroundColor:"#ECF5FF",
+    alignItems:"center",justifyContent:"center",flexDirection:"column"}}>
     <Box component="h2"> uplaod your files here</Box>
     <Button size='large' variant="contained" component="label" sx={{width:"150px",height:"60px",color:"white"}}> 
     Upload File
@@ -57,8 +58,8 @@ const Tablecomponent=()=>{
  
 {uplaodToBlockchainBoxes()}
     </div>
+    {displayUplaodTable()}
     </>
   )
 }
 export default Tablecomponent;
-{/*  */}

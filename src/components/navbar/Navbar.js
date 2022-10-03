3d612543a238f63ed8ab8296ccb3e26bd2eddb33
Usi Mlonzi <usimancobo@gmail.com>
2022-10-03 13:51:25 +0200

    first commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './navbar.css'
+//  import logo from '../../Images/pixlr-bg-result (1).png';
+
+ 
+function Navbar() {
+    return (
+        <>
+        <div className='backgroundimg'>
+            <nav>
+        {/* <img className='logo'   src={logo} alt="logo"/> */}
+        
+        <ul>
+            <li>
+                <a href=''>Home</a>
+            </li>
+            <li>
+
+                <a href='/'>About</a>
+            </li>
+           
+            <li>
+                <a href=''>Login/Register</a>
+            </li>
+        </ul>
+        </nav>
+        <h1>DocumentV</h1>
+        <p>Quisque interfum nibh quis scelerisque mollis. <br></br> Etiam et sem vehicula, portitor ipsum eu, vehicula lacus</p>
+         <Button>
+            Login
+         </Button>
+         </div>
+         
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/pages/landingPage/LandingPage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar';
+// import './home.css'
+// import About from '../../components/about/About';
+// import Lodge from '../../components/rooms/Lodge';
+// import Activities from '../../components/activities/Activities';
+// import Footer from '../../components/footer/Footer';
+
+function LandingPage() {
+    return (
+    <div className='nav'>
+    
+      <Navbar/>
+      {/* <About/>
+      <Lodge/>
+      <Activities/>
+      <Footer/> */}
+    </div>
+  )
+}
+
+export default LandingPage

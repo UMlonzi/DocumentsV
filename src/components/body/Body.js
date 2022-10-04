b5aae5e76da3b180e681b1d27d21924909019ce6
Usi Mlonzi <usimancobo@gmail.com>
2022-10-04 09:41:16 +0200

    saPush

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -19,6 +19,9 @@ function Body(){
         <div className='box2'>
         <h2> We will collaborate with you</h2>
         <div className='line'/>
+        <p className='p2'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
         </div>
          </div>
          

99d78a2b1602da24a89f2fe3e36e1bcfeeaee960
Usi Mlonzi <usimancobo@gmail.com>
2022-10-04 15:01:36 +0200

    changes

--- src/pages/landingPage/LandingPage.js
+++ src/pages/landingPage/LandingPage.js
@@ -3,13 +3,8 @@ import Navbar from '../../components/navbar/Navbar';
 import Banner from '../../components/banner/Banner'
 // import './home.css'
  import Body from '../../components/body/Body';
-<<<<<<< HEAD
- import Banner from '../../components/banner/Banner';
-//  import Footer from '../../components/footer/Footer';
-=======
 // import Lodge from '../../components/rooms/Lodge';
 // import Activities from '../../components/activities/Activities';
->>>>>>> e6d96bc6c3d49ed0d5baf1c1823f365125232bae
 
 // import Footer from '../../components/footer/Footer';
 
@@ -21,10 +16,7 @@ function LandingPage() {
       <Navbar/>
     <Body/>
     <Banner/>
-<<<<<<< HEAD
     {/* <Footer/> */}
-=======
->>>>>>> e6d96bc6c3d49ed0d5baf1c1823f365125232bae
     </div>
 
     )

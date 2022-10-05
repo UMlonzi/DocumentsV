5eed076ea41c926f5b3d92a9ab60bf598b702e72
Mvuleni <mkekana07@gmail.com>
2022-10-05 14:28:47 +0200

    navigationbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Authentication from './pages/authentictaion/Authentication';
 import Profile from './pages/profile/Profile';
 import LandingPage from './pages/landingPage/LandingPage';
+import Navigation from './pages/navigationbar/Navigation';
 import { BrowserRouter ,Routes,Route } from 'react-router-dom';
 
 
@@ -14,10 +15,11 @@ function App() {
    <Routes>
     <Route path='/' exact element={<LandingPage/>}/>
     <Route path='/Authentication' element={<Authentication/>}/>
-    <Route path='/Profile' element={<Profile/>}/>      
+    <Route path='/Profile' element={<Profile/>}/> 
+    <Route path='/Navigation' element={<Navigation/>} />     
+    
    </Routes>
    </BrowserRouter>
-   <Authentication/>
     </div>
   );
 }

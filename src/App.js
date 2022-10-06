da4ea57fd7cfa0c2c7163f0c4aa43a209ac7af76
Usi Mlonzi <usimancobo@gmail.com>
2022-10-06 10:48:57 +0200

    push

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@
 import Authentication from './pages/authentictaion/Authentication';
 import Profile from './pages/profile/Profile';
 import LandingPage from './pages/landingPage/LandingPage';
-<<<<<<< HEAD
+
 
 // import Navigation from './pages/navigationbar/Navigation';
-=======
 import fandingPage from './pages/landingPage/LandingPage';
-import Navigation from './pages/navigationbar/Navigation';
+// import Navigation from './pages/navigationbar/Navigation';
 import { BrowserRouter ,Routes,Route } from 'react-router-dom';
 //import Footer from './pages/footer/footer';
 //import './pages/footer.css'; 
 
->>>>>>> fc4b2c5dee5c3d2d84624dfb9860fc869803c939
 
 
 
@@ -28,10 +26,7 @@ function App() {
    <Route path='/' exact element={<LandingPage/>}/>
     <Route path='/Authentication' element={<Authentication/>}/>
     <Route path='/Profile' element={<Profile/>}/> 
-<<<<<<< HEAD
     {/* <Route path='/Navigation' element={<Navigation/>} />   */}
-=======
->>>>>>> fc4b2c5dee5c3d2d84624dfb9860fc869803c939
     <Route path="/Dashboard" element={<DashBoardPage/>}/>
     <Route path='/Profile' element={<Profile/>}/>      
     <Route  path='veriAuthentication' exact element={<VeriAuthentication/>}/>

82ac85bf308d27f0f7238fb7be0ad1b7432d9406
Kris-fynn <prokris.fynn@gmail.com>
2022-10-18 13:54:07 +0200

    commit

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -19,7 +19,7 @@ const Modal = ({ open, onClose }) => {
           <div className='content'>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             <h1>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </h1>
-            <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+            <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
           </div>
           <div className='btnContainer'>
             <button className='btnPrimary'>
